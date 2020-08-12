055f4dbc8f95f469314d749c16c4efd3b4d3429e
Tiago Souza <tiago.souza@zup.com.br>
2020-08-12 13:43:01 -0300

    Projeto plataforma de estudos

---
+++ src/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const server = express();
+
+const { pageLanding, pageStudy, pageGiveClasses, saveClasses, pageSuccess} = require('./pages');
+
+// Set up nunjucks (template engine)
+const nunjucks = require('nunjucks');
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true
+});
+
+// Sever start and configuration
+server
+// Receives req.body data
+.use(express.urlencoded({extended: true}))
+// Set up files statics
+.use(express.static('public'))
+// Application routes
+.get('/', pageLanding)
+.get('/study', pageStudy)
+.get('/give-classes', pageGiveClasses)
+.post('/save-classes', saveClasses)
+.get('/page-success', pageSuccess)
+// Start the server
+.listen(5500);
